e8c9026948bdb1bd3fe43b6090320a259b30f38b
Green-Avocado <greenavocado@protonmail.com>
2020-08-07 21:41:35 -0600

    feat: working dad bot

--- index.js
+++ index.js
@@ -3,14 +3,72 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+function dad_joke(msg, n) {
+    if(n != -1) {
+        content = msg.content.substring(n);
+
+        var ends = [
+            content.indexOf("\n"),
+            content.indexOf("."),
+            content.indexOf(",")
+        ];
+
+        var m = -1;
+
+        for(let i = 0; i < ends.length; i++) {
+            if(m == -1 || (ends[i] < m && ends[i] != -1)) {
+                m = ends[i];
+            }
+        }
+
+        var name;
+
+        if(m == -1) {
+            name = content;
+        }
+        else {
+            name = content.substring(m, -1);
+        }
+
+        msg.reply(`Hi ${name}, I'm dad!`);
+    }
+}
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    console.log(msg.content);
+
+    var s = msg.content.toLowerCase();
+
+    if(!msg.author.bot) {
+        if(s.startsWith("im ")) {
+            dad_joke(msg, 3);
+        }
+        else if(s.startsWith("i'm ")) {
+            dad_joke(msg, 4);
+        }
+        else if(s.includes(" im ") || s.includes(" i'm ")) {
+            var n = -1;
+
+            if(
+                s.indexOf(" i'm ") == -1
+                || (
+                    s.indexOf(" im ") != -1
+                    && s.indexOf(" im ") < s.indexOf(" i'm ")
+                )
+            ) {
+                n = s.indexOf(" im ") + 4; 
+            }
+            else {
+                n = s.indexOf(" i'm ") + 5; 
+            }
+
+            dad_joke(msg, n);
+        }
+    }
 });
 
 client.login(process.env.TOKEN);
